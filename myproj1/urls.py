9206a5b91b4856055e8bc658e751562caeeb8b60
root <joelau94@gmail.com>
2015-11-22 01:03:57 +0800

    JoeLau20151121

---
+++ myproj1/urls.py
@@ -0,0 +1,34 @@
+"""myproj1 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add an import:  from blog import urls as blog_urls
+    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+"""
+#from django.conf.urls import include, url
+from django.contrib import admin
+from django.conf.urls import *
+from myproj1.view import *
+from myproj1 import settings
+
+urlpatterns = patterns("",
+	(r'^index/$', index),
+	(r'^login/$', login),
+	(r'^register/$', register),
+	(r'^hospitals/$', hospitals),
+	(r'^orders/$', orders),
+	(r'^checkout/$', checkout),
+	(r'^contact/$', contact),
+)
+
+# urlpatterns = [
+#     url(r'^admin/', include(admin.site.urls)),
+# ]
